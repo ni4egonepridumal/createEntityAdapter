f8b2169ac48e9a57bd1d3e0e606efa1280799404
Максим <shal@intersvyaz.net>
2024-11-03 22:47:00 +0500

    поправил чуть правила для линтеров

--- src/features/test/testSlice.ts
+++ src/features/test/testSlice.ts
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 
 const userAdapter = createEntityAdapter({
-  //@ts-expect-error
-  selectId: (user) => user.id, // поле идентифиактора сущности — это uuid
-  //@ts-expect-error
+  //@ts-ignore
+  selectId: (user) => user.id,
+  //@ts-ignore
   sortComparer: (a, b) => a.title.localeCompare(b.title),
 });
 
@@ -14,6 +12,7 @@ export const userSlice = createSlice({
   initialState: userAdapter.getInitialState(),
   reducers: {
     userAdd: userAdapter.addOne,
+    //@ts-ignore
     userReceived(state, action) {
       console.log("action", action.payload);
       userAdapter.setAll(state, action.payload);
